5dd1a93ef3db606c42bcc59f108d4de8183b83f3
thobifritz <thobifritz@gmail.com>
2026-03-05 15:43:45 +0100

    Add files via upload

---
+++ PrivateMediathekApp_Project/app/src/main/java/com/example/privatemediathek/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.privatemediathek
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(color = MaterialTheme.colorScheme.background) {
+                Text(text = "Private Mediathek App - Grundgerüst erstellt!")
+            }
+        }
+    }
+}
